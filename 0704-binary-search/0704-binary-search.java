c55042663dadcd00a572f273ac406d930542a0e2
Keianna <124526165+Keianna-B@users.noreply.github.com>
2024-02-20 22:40:30 -0600

    Time: 0 ms (100%), Space: 45.5 MB (66.62%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0, right = nums.length -1;
+        //int index;
+        while(left <= right){
+            int index = left+ (right-left)/2;
+            if (nums[index] == target){
+                return index;
+            }
+            else if(nums[index] < target){
+                left = index + 1;
+            }
+            else{
+                right = index - 1;
+            }
+        }
+
+        return -1;
+    }
+}
