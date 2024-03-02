69742c997abeb56225e726cf9ff7860f4a2d9dcd
Keianna <124526165+Keianna-B@users.noreply.github.com>
2024-03-02 11:09:35 -0600

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -4,7 +4,39 @@ class Solution {
         {
             nums[i] *= nums[i];        
         }
-        Arrays.sort(nums);
-        return nums;
+        return countingSort(nums);
     }
+
+    public static int[] countingSort(int[] input){
+        int length = input.length;
+        int maximum = 0;
+
+        for(int i = 0; i < length; i++)
+        {
+            maximum = Math.max(maximum, input[i]);
+        }
+
+        int[] count = new int[maximum+1];
+
+        for (int i = 0; i < length; i++) 
+        {
+            count[input[i]]++;
+        }
+ 
+        for (int i = 1; i <= maximum; i++) 
+        {
+            count[i] += count[i - 1];
+        }
+
+        int [] output = new int [length];
+
+        for (int i = length - 1; i >= 0; i--) 
+        {
+            output[count[input[i]] - 1] = input[i];
+            count[input[i]]--;
+        }
+ 
+        return output;
+    }
+
 }
