a0992e2145b80adb901f1df919880f676ed16b0e
Keianna <124526165+Keianna-B@users.noreply.github.com>
2024-03-18 09:50:25 -0500

    Time: 2 ms (78.54%), Space: 44.8 MB (30.33%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,12 +1,22 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        Set seen = new HashSet();
-        for (int i=0; i<9; ++i) {
-            for (int j=0; j<9; ++j) {
-                if (board[i][j] != '.') {
-                    String num = "(" + board[i][j] + ")";
-                    if (!seen.add(num + i) || !seen.add(j + num) || !seen.add(i/3 + num + j/3))
-                        return false;
+        Set<Character>[] rows = new HashSet[9];
+        Set<Character>[] columns = new HashSet[9];
+        Set<Character>[] boxes = new HashSet[9];
+
+        for (int i = 0; i < 9; i++) {
+            rows[i] = new HashSet<>();
+            columns[i] = new HashSet<>();
+            boxes[i] = new HashSet<>();
+        }
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                char digit = board[i][j];
+                if (digit == '.')
+                    continue;
+                if (!rows[i].add(digit) || !columns[j].add(digit) || !boxes[(i / 3) * 3 + j / 3].add(digit)) {
+                    return false;
                 }
             }
         }
