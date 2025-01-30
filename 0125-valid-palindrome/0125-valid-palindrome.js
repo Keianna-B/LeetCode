6c8ffe260c43b64a669a1a7812236585e084def6
Keianna <124526165+Keianna-B@users.noreply.github.com>
2025-01-30 15:17:32 -0600

    Time: 5 ms (59.83%), Space: 51 MB (93.03%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -3,11 +3,10 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-    s.replace(/[^a-zA-Z0-9]/g, '');
-    s.toLowerCase();
+    s = s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
     let p1 = 0;
     let p2 = s.length-1;
-    while(p1!= p2){
+    while(p1 < p2){
         if(s.charAt(p1) !== s.charAt(p2)){
             return false;
         }
