fb0086ade0dbb1bbddfeba402d84880dc795ad7e
Keianna <124526165+Keianna-B@users.noreply.github.com>
2025-01-30 15:07:35 -0600

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    s.replace(/[^a-zA-Z0-9]/g, '');
+    s.toLowerCase();
+    let p1 = 0;
+    let p2 = s.length-1;
+    while(p1!= p2){
+        if(s.charAt(p1) !== s.charAt(p2)){
+            return false;
+        }
+        p1++;
+        p2--;
+    }
+    return true;
+};
